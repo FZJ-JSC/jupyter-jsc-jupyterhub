897a0fe1b9db969ce25ee5a03ada8d39d2cf6f8a
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2022-10-11 13:52:52 +0200

    add TemplateServerHandler

--- custom/2.1.1/handler/page_handlers.py
+++ custom/2.1.1/handler/page_handlers.py
@@ -1,8 +1,10 @@
+import json
 import os
-from tornado import web
+
 from custom_utils import get_vos
 from jupyterhub.handlers.base import BaseHandler
 from jupyterhub.scopes import needs_scope
+from tornado import web
 
 
 async def get_user_auth_state_with_vos(user):
@@ -30,7 +32,7 @@ class LinksHandler(BaseHandler):
         html = await self.render_template("links.html", **ns)
         self.finish(html)
 
-     
+
 class TwoFAHandler(BaseHandler):
     async def get(self):
         user = self.current_user
@@ -45,7 +47,7 @@ class ImprintHandler(BaseHandler):
         ns = await _create_ns(user)
         html = await self.render_template("imprint.html", **ns)
         self.finish(html)
-    
+
 
 class DPSHandler(BaseHandler):
     async def get(self):
@@ -69,7 +71,12 @@ class LoggingHandler(BaseHandler):
     async def get(self):
         user = self.current_user
         ns = await _create_ns(user)
-        ns.update({'show_drf_logs': os.environ.get("SHOW_DRF_LOGS", "false").lower() in ["true", "1"]})
+        ns.update(
+            {
+                "show_drf_logs": os.environ.get("SHOW_DRF_LOGS", "false").lower()
+                in ["true", "1"]
+            }
+        )
         html = await self.render_template("logging.html", **ns)
         self.finish(html)
 
@@ -80,7 +87,9 @@ class VOHandler(BaseHandler):
         user = self.current_user
         auth_state = await user.get_auth_state()
         custom_config = user.authenticator.custom_config
-        vo_active, vo_available = get_vos(auth_state, custom_config, user.name, user.admin)
+        vo_active, vo_available = get_vos(
+            auth_state, custom_config, user.name, user.admin
+        )
         auth_state["vo_active"] = vo_active
         auth_state["vo_available"] = vo_available
         await user.save_auth_state(auth_state)
@@ -88,9 +97,8 @@ class VOHandler(BaseHandler):
         vo_details_config = custom_config.get("vos", {})
         vo_details = {}
         for vo_name in vo_available:
-            vo_details[vo_name] = (
-                vo_details_config.get(vo_name, {})
-                .get("description", "No description available")
+            vo_details[vo_name] = vo_details_config.get(vo_name, {}).get(
+                "description", "No description available"
             )
 
         html = await self.render_template(
@@ -99,5 +107,34 @@ class VOHandler(BaseHandler):
             auth_state=auth_state,
             vo_active=vo_active,
             vo_details=vo_details,
+        )
+        self.finish(html)
+
+
+class TemplateServerHandler(BaseHandler):
+    @web.authenticated
+    async def post(self, template):
+        user = self.current_user
+        active_server = [
+            (k, v.user_options.get("name", k))
+            for k, v in user.spawners.items()
+            if v.ready
+        ]
+        args = self.request.arguments
+        try:
+            args_params = args["params"]
+            if type(args_params) == list:
+                args_params = args_params[0]
+            template_params = json.loads(args_params.decode())
+        except:
+            self.log.exception("Could not read template parameters")
+            template_params = {}
+
+        html = await self.render_template(
+            "notebook_template_server.html",
+            user=user,
+            template=template,
+            template_params=template_params,
+            active_server=active_server,
         )
         self.finish(html)

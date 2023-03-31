36ca81b9cbe6c84d606b5ea77983fe5c5774f4d8
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-03-31 09:13:17 +0200

    fix sed command

--- devel/entrypoint.sh
+++ devel/entrypoint.sh
@@ -28,7 +28,7 @@ cp -r /usr/local/etc/jupyterhub/jupyterhub_config.py /home/${USERNAME}/jupyterhu
 cp -r /usr/local/etc/jupyterhub/z2jh.py /home/${USERNAME}/jupyterhub-config/.
 cp -r /usr/local/etc/jupyterhub/secret/..data/* /home/${USERNAME}/jupyterhub-config/secret/.
 cp -r /usr/local/etc/jupyterhub/config/..data/* /home/${USERNAME}/jupyterhub-config/config/.
-sed -i -e 's@/usr/local/etc/jupyterhub@/home/${USERNAME}/jupyterhub-config/g' /home/${USERNAME}/jupyterhub-config/*
+sed -i -e 's@/usr/local/etc/jupyterhub@/home/${USERNAME}/jupyterhub-config@g' /home/${USERNAME}/jupyterhub-config/*
 
 
 chown -R ${USERNAME}:users /home/${USERNAME}

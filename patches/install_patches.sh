c2f5b8716095ef581788f059397d2c68f4e4fd23
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2025-11-10 11:26:57 +0100

    Add traefik installation

--- patches/install_patches.sh
+++ patches/install_patches.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 for f in `ls /src/patches/patch_files/*.patch`
 do
-    echo "Apply patch $(basename $f)"
+    echo "JupyterHub: Apply patch $(basename $f)"
+    patch -d /src/ -p1 < $f
+done
+for f in `ls /src/patches/patch_files_traefik/*.patch`
+do
+    echo "Traefik: Apply patch $(basename $f)"
     patch -d /src/ -p1 < $f
 done
 pip3 install -e /src/jupyterhub
+pip3 install -e /src/traefik-proxy

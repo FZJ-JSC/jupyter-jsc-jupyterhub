15e9ed68f74f72a7e517acb466bd7810a0c73485
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2025-11-17 09:35:28 +0100

    remove traefik integration

--- patches/install_patches.sh
+++ patches/install_patches.sh
@@ -4,10 +4,4 @@ do
     echo "JupyterHub: Apply patch $(basename $f)"
     patch -d /src/ -p1 < $f
 done
-for f in `ls /src/patches/patch_files_traefik/*.patch`
-do
-    echo "Traefik: Apply patch $(basename $f)"
-    patch -d /src/ -p1 < $f
-done
 pip3 install -e /src/jupyterhub
-pip3 install -e /src/traefik-proxy

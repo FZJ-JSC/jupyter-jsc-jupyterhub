831b8397867b1a2b06ec26f32e44bc31093aa007
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2026-03-18 17:37:06 +0100

    update sidecar image

--- sidecar/restart_hub_init.sh
+++ sidecar/restart_hub_init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd /mnt/shared-data ;
-export GIT_REPO_SHORT=${GIT_REPO#"https://"} ; 
-git clone --single-branch --branch ${GIT_BRANCH} https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPO_SHORT} static-files 
+cd /mnt/shared-data
+export GIT_REPO_SHORT=${GIT_REPO#"https://"}
+if [[ -n ${GIT_USERNAME} && -n ${GIT_PASSWORD} ]]; then
+    git clone --single-branch --branch ${GIT_BRANCH:-main} https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPO_SHORT} static-files
+else
+    git clone --single-branch --branch ${GIT_BRANCH:-main} https://${GIT_REPO_SHORT} static-files
+fi

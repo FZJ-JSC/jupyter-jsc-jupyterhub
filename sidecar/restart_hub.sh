8affbab800e931624b5b6249811fca29e83b6783
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2026-03-20 10:32:36 +0100

    use new jsc custom package

--- sidecar/restart_hub.sh
+++ sidecar/restart_hub.sh
@@ -2,4 +2,5 @@
 kubectl patch service hub -n ${TARGET_NAMESPACE} -p '{"spec":{"selector":{"component":"maintenance"}}}'
 kubectl rollout restart deployment hub -n ${TARGET_NAMESPACE}
 kubectl rollout status deployment hub -n ${TARGET_NAMESPACE}
+sleep 10
 kubectl patch service hub -n ${TARGET_NAMESPACE} -p '{"spec":{"selector":{"component":"hub"}}}'
